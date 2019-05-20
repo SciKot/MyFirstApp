e120f177d0749b4610ed1a181c727f22c124bcc1
SciKot <mr.dobrolubov@gmail.com>
2019-05-20 14:46:44 +0400

    Week3: before task1

---
+++ app/src/main/java/kushnirmaxim/myfirstapp/AuthActivity.java
@@ -0,0 +1,68 @@
+package kushnirmaxim.myfirstapp;
+
+import android.support.annotation.StringRes;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class AuthActivity extends AppCompatActivity {
+
+    private EditText mLogin;
+    private EditText mPassword;
+    private Button mEnter;
+    private Button mRegister;
+
+    private View.OnClickListener mOnEnterClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            if (isEmailValid()) {
+                //переход в приложение
+            }
+        }
+    };
+
+    private View.OnClickListener mOnRegisterClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            if (isEmailValid() && isPasswordValid()) {
+                //переход в приложение
+            } else {
+                showMessage(R.string.input_error);
+            }
+        }
+    };
+
+    private boolean isEmailValid() {
+        return !TextUtils.isEmpty(mLogin.getText())
+                && Patterns.EMAIL_ADDRESS.matcher(mLogin.getText()).matches();
+    }
+
+    private boolean isPasswordValid() {
+        return !TextUtils.isEmpty(mPassword.getText());
+    }
+
+    private  void showMessage(@StringRes int string) {
+        Toast.makeText( this, string, Toast.LENGTH_LONG).show();
+    }
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ac_auth);
+
+        mLogin = findViewById(R.id.etLogin);
+        mPassword = findViewById(R.id.etPassword);
+        mEnter = findViewById(R.id.buttonEnter);
+        mRegister = findViewById(R.id.buttonRegister);
+
+        mEnter.setOnClickListener(mOnEnterClickListener);
+        mRegister.setOnClickListener(mOnRegisterClickListener);
+
+    }
+}
